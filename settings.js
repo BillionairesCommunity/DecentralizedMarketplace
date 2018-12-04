7f0c05fe812c70564ebd863283e9da4260517202
Rostyslav Bortman <bortmanrostyslav@gmail.com>
2018-12-04 18:22:48 +0200

    connection to mongoDB added

--- settings.js
+++ settings.js
@@ -1,2 +1,18 @@
-var settings = module.exports = {};
-
+// Default Options:
+module.exports = {
+    debug: false,
+    host: 'localhost',
+    port: 27017,
+    database: 'Test',
+    connectOptions: {
+        db: {},
+        server: {
+            auto_reconnect: true
+        },
+        replset: {},
+        user: {},
+        pass: {},
+        auth: {},
+        mongos: {}
+    }
+};
